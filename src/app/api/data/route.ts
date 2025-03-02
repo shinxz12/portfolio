897fb8f2079be59d60485840817ed243e674afe7
shinxz12 <ngocbthe@gmail.com>
2025-03-02 22:26:44 +0700

    style: fix no use variable

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+
+export async function GET() {
   return NextResponse.json({
     success: true,
     message: 'hle!',
